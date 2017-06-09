4039f362bfcc30cc33d7ec72df97f3d23061927f
Zainab Hammami <zainab.hammami.zh@gmail.com>
2017-06-10 02:16:59 +0300

    socket.io

--- server.js
+++ server.js
@@ -1,2 +1,29 @@
-var express = require ("express");
-var socket = require ("socket");
+var app = require ("express")();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+io.on('connection', function(socket){
+	console.log('a user connected');
+	socket.on('disconnect', function() {
+		console.log('user is disconnected now')
+	});
+});
+
+app.use(bodyParser.json());
+
+// Connect to Mongoose
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/chat-app');
+var db = mongoose.connection;
+console.log("mongodb is now connected to the serever");
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(process.env.PORT || 3000);
+console.log("app is listening on port 3000");
+
+module.exports = app;
